8ca6458d92d5b7d18286a189d469ebc33dddac25
Agus-Coder <agus_code@outlook.com.ar>
2024-01-15 14:51:59 -0300

    guia 1

---
+++ 04-lnguaje-python/notes.py
@@ -0,0 +1,53 @@
+### Variables
+# Numeros naturales
+num = 15;
+
+# Numeros reales
+real = 0.15632;
+
+# Notacion Cientifica
+cientifica = 0.1e-4
+
+# Operaciones
+
+suma = num + real
+resta = num - real
+multi = num * real
+expor = real ** num
+div = num / real
+div_entera = num // real
+modulo = num % real # esto seria el resto que te queda de la division
+
+# operadores relacionales
+
+# Mayor
+num > real
+
+# Mayor o igual que
+num >= real
+
+# menor 
+
+# menor o igual que
+# igual
+# distinto
+#
+
+
+# Print en pantalla
+print("Esto es un mensaje")
+
+a = int(input("Ingrese un primer numero"))
+b = int(input("Ingrese un segundo numero"))
+c = a + b
+
+print("la suma entre %d y %d es %d" %(a,b,c))
+
+# Comentario simple
+
+"""
+Comentario
+multi
+linea
+:)
+"""
