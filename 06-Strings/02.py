7946219b0e3174e781140945bee15470f97f0816
Agus-Coder <agus_code@outlook.com.ar>
2024-02-07 15:58:02 -0300

    End 6th module

--- 06-Strings/02.py
+++ 06-Strings/02.py
@@ -1,3 +1,11 @@
 cadena = input("Ingrese algo con mas de 5 caracteres ");
 
-print(cadena[0:2])
+print(cadena[0:2]) # el ultimo espacio no es considerado
+
+leng = len(cadena)
+al_reves = ''
+for x in range(-1, -leng, -1):
+    al_reves += cadena[x]
+
+print(al_reves)
+
---
+++ 06-Strings/08.py
@@ -0,0 +1,5 @@
+cadena = input("Ingrese algo con mas de 5 caracteres ")
+
+cadena2 = cadena.replace(" ", "&")
+
+print(cadena2)
